7f75a56c477292f9e9f9efae5f9fad6cd28039cf
zhongya.yan <zhongya.yan@ly.com>
2020-05-08 13:50:10 +0800

    创建 Socker 服务器和客户端连接

--- main/main.cc
+++ main/main.cc
@@ -20,6 +20,11 @@
 #define QUEUE 20 //连接请求队列
 #endif
 
+#ifndef CLIENT_INFO_
+#define MYPORT 7000
+#define BUFFER_SIZE 1024
+#endif
+
 int main(int argc, char **argv) {
   /**
    * Socket 服务端编程
@@ -101,5 +106,43 @@ int main(int argc, char **argv) {
    * 关闭监听
    * */
   close(fd);
+  return 0;
+}
+
+int client() {
+  ///定义sockfd
+  int sock_cli = socket(AF_INET, SOCK_STREAM, 0);
+
+  ///定义sockaddr_in
+  struct sockaddr_in servaddr;
+  memset(&servaddr, 0, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(MYPORT); //服务器端口
+  servaddr.sin_addr.s_addr = inet_addr(
+      "127.0.0.1"); //服务器ip，inet_addr用于IPv4的IP转换（十进制转换为二进制）
+  // 127.0.0.1是本地预留地址
+  //连接服务器，成功返回0，错误返回-1
+  if (connect(sock_cli, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+    perror("connect");
+    exit(1);
+  }
+
+  char sendbuf[BUFFER_SIZE];
+  char recvbuf[BUFFER_SIZE];
+
+  while (
+      fgets(sendbuf, sizeof(sendbuf), stdin) !=
+      NULL) { /*每次读取一行，读取的数据保存在buf指向的字符数组中，成功，则返回第一个参数buf；*/
+    send(sock_cli, sendbuf, strlen(sendbuf), 0); ///发送
+    if (strcmp(sendbuf, "exit\n") == 0)
+      break;
+    recv(sock_cli, recvbuf, sizeof(recvbuf), 0); ///接收
+    fputs(recvbuf, stdout);
+
+    memset(sendbuf, 0,
+           sizeof(sendbuf)); //接受或者发送完毕后把数组中的数据全部清空（置0）
+    memset(recvbuf, 0, sizeof(recvbuf));
+  }
+  close(sock_cli);
   return 0;
 }

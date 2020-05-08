6e9d54ac2ac22d8726cb15505a69bb7936ddf20f
zhongya.yan <zhongya.yan@ly.com>
2020-05-08 18:39:46 +0800

    非阻塞单连接

--- main/main.cc
+++ main/main.cc
@@ -9,7 +9,6 @@
 #include <sys/shm.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <thread>
 #include <unistd.h>
 
 #ifndef PORT
@@ -26,123 +25,207 @@
 #endif
 
 int main(int argc, char **argv) {
-  /**
-   * Socket 服务端编程
-   *
-   * */
-  // 创建 Socket
-  // socket(domain, type, protocol)
-  // 可以参考： https://www.geeksforgeeks.org/socket-programming-cc/
-  int fd = socket(AF_INET, SOCK_STREAM, 0); // 返回一个 Socket 文件描述符号
-  if (fd == 0) {
-    std::cout << "application Socket source fail" << std::endl;
-    return 0;
-  }
-  /*
-   * 设置 Socket 属性
-   * */
+  fd_set rdfs;
 
-  /*
-   * 绑定 IP 端口
+  /**
+   * */
+  struct timeval tv;
+
+  /**
+   * */
+  int retval, maxfd;
+
+  /**
+   * 新建一个 Socket 文件描述符
+   * */
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  /**
+   * 新建 address
    * */
   struct sockaddr_in server_address;
-  // 绑定端口，Htonl 将一个无符号短整型数值转换为网络字节序，即大端模式
+  // 初始化
+  bzero(&server_address, sizeof(server_address));
+  server_address.sin_addr.s_addr = htonl(INADDR_ANY);
   server_address.sin_port = htons(PORT);
-  // INADDR_ANY：任意地址类型，也可以指定：inet_addr("127.0.0.1");
-  server_address.sin_addr.s_addr = INADDR_ANY;
-  // 使用 IP V4 地址
   server_address.sin_family = AF_INET;
-  // int bind(int sockfd, const struct sockaddr *addr,  socklen_t addrlen);
-  int result =
-      bind(fd, (struct sockaddr *)&server_address, sizeof(server_address));
-  if (result < 0) {
-    std::cout << "bind fail" << std::endl;
+  /**
+   * bind Socket
+   * */
+  int b = bind(fd, (struct sockaddr *)&server_address, sizeof(server_address));
+  if (b < 0) {
+    std::cout << "绑定失败" << std::endl;
+    perror("bind");
     return 0;
-  }
-  /*
-   * 开启监听
-   * */
-  // int listen(int sockfd, int backlog);
-  // 第一个参数是当前 SOcket 文件描述符，第二个参数是 当前可以阻塞的队列大小
-  int l = listen(fd, QUEUE);
-  if (l < 0) {
-    std::cout << "listen fail" << std::endl;
-  }
-  /*
-   * 接收客户端传过来的连接
-   * */
-  struct sockaddr_in client_addr;
-  socklen_t length = sizeof(client_addr);
-  // accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
-  // 成功返回非负描述字，出错返回-1
-  int clientfd = accept(fd, (struct sockaddr *)&client_addr, &length);
-  // 如果accpet成功，那么其返回值是由内核自动生成的一个全新描述符，代表与所返回客户的TCP连接。
-  if (clientfd < 0) {
-    std::cout << "accept fail" << std::endl;
-    return 0;
-  }
-  /*
-   * 收发数据
-   * */
-  // 设置缓冲区
-  char buffer[1024];
-  while (1) {
-    // 缓冲区设置为空
-    bzero(buffer, sizeof(buffer));
-    int len = read(clientfd, buffer, sizeof(buffer) - 1);
-    if (len < 0) {
-      std::cout << "remote server close" << std::endl;
-      break;
-    }
-    std::cout << "recv message:" << buffer << std::endl;
-    send(clientfd, "hello", strlen("hello"), 0);
   }
 
-  /*
-   * 关闭 Socket
+  /**
+   * 监听Socket ，并设置阻塞队列
    * */
-  close(clientfd);
-  /*
-   * 关闭监听
+  int l = listen(fd, QUEUE);
+  if (l < 0) {
+    std::cout << "监听失败" << std::endl;
+    perror("listen");
+    return 0;
+  }
+
+  /**
+   * 给客户端分配 socket
+   * */
+  struct sockaddr_in client_socker;
+  socklen_t client_socket_length = sizeof(client_socker);
+  /**
+   * accept 等待连接
+   * */
+  int a = accept(fd, (struct sockaddr *)&client_socker, &client_socket_length);
+  if (a < 0) {
+    std::cout << "accept 失败" << std::endl;
+    perror("accept");
+    return 0;
+  }
+  /**
+   * 等待连接进来
+   * */
+  while (1) {
+    // 清空可读文件
+    FD_ZERO(&rdfs);
+    // 标准输入文件描述符，添加到文件集合
+    FD_SET(0, &rdfs);
+    maxfd = 0;
+    // 添加当前连接添加到集合
+    FD_SET(a, &rdfs);
+    if (maxfd < a) {
+      maxfd = a;
+    }
+
+   // 设置超时时间
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+    // 异步监听进来的连接
+    retval = select(maxfd + 1, &rdfs, NULL, NULL, &tv);
+    // 连接返回信息
+    if (retval < 0) {
+      std::cout << "select Fail" << std::endl;
+      break;
+    } else if (retval == 0) {
+      std::cout << "read message is empty" << std::endl;
+    } else {
+      std::cout << "have message go up up " << std::endl;
+      // 判断是否是客户发来的信息
+      if (FD_ISSET(a, &rdfs)) {
+        char buffer[1024];
+        // 清空缓存
+        bzero(&buffer, sizeof(buffer));
+        int count = recv(a, buffer, sizeof(buffer), 0);
+        if (count < 0) {
+          std::cout << "接收信息失败" << std::endl;
+        }
+        std::cout << "recv message:" << buffer << std::endl;
+        
+        if (strcmp(buffer,"you zenme bufa message") == 0) {
+          char buff[0];
+          ssize_t s = send(a, buff, sizeof(buff), 0);
+          if (s < 0) {
+            std::cout << "send message  Fila" << std::endl;
+          }
+        } else {
+          // 用户发送过来信息，回复信息
+          if (FD_ISSET(a, &rdfs)) {
+            char buff[1024] = "dsafasfasfsaf";
+            ssize_t s = send(a, buff, sizeof(buff), 0);
+            if (s < 0) {
+              std::cout << "send message  Fila" << std::endl;
+            }
+          }
+        }
+
+      } else {
+        std::cout << "is Not Client Send Message" << std::endl;
+      }
+    }
+  }
+
+  /**
+   * 关闭连接
+   * */
+  close(a);
+  /**
+   * 关闭文件描述符
    * */
   close(fd);
+
   return 0;
 }
 
-int client() {
-  ///定义sockfd
-  int sock_cli = socket(AF_INET, SOCK_STREAM, 0);
 
-  ///定义sockaddr_in
-  struct sockaddr_in servaddr;
-  memset(&servaddr, 0, sizeof(servaddr));
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_port = htons(MYPORT); //服务器端口
-  servaddr.sin_addr.s_addr = inet_addr(
-      "127.0.0.1"); //服务器ip，inet_addr用于IPv4的IP转换（十进制转换为二进制）
-  // 127.0.0.1是本地预留地址
-  //连接服务器，成功返回0，错误返回-1
-  if (connect(sock_cli, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-    perror("connect");
-    exit(1);
-  }
+int client(){
 
-  char sendbuf[BUFFER_SIZE];
-  char recvbuf[BUFFER_SIZE];
 
-  while (
-      fgets(sendbuf, sizeof(sendbuf), stdin) !=
-      NULL) { /*每次读取一行，读取的数据保存在buf指向的字符数组中，成功，则返回第一个参数buf；*/
-    send(sock_cli, sendbuf, strlen(sendbuf), 0); ///发送
-    if (strcmp(sendbuf, "exit\n") == 0)
-      break;
-    recv(sock_cli, recvbuf, sizeof(recvbuf), 0); ///接收
-    fputs(recvbuf, stdout);
+    int sock_cli;
+    fd_set rdfs;
+    struct timeval tv;
 
-    memset(sendbuf, 0,
-           sizeof(sendbuf)); //接受或者发送完毕后把数组中的数据全部清空（置0）
-    memset(recvbuf, 0, sizeof(recvbuf));
-  }
-  close(sock_cli);
-  return 0;
+    int retval,maxfd;
+
+    // 新建 socket 文件
+    sock_cli = socket(AF_INET,SOCK_STREAM,0);
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(MYPORT);
+    server_addr.sin_family = AF_INET;
+
+    int connection = connect(sock_cli,(struct sockaddr*)&server_addr,sizeof(server_addr));
+    if (connection < 0 ){
+        std::cout << "connection 失败" <<std::endl;
+        perror("connection");
+        return 0;
+    }
+
+    while (1){
+        /*把可读文件描述符的集合清空*/
+        FD_ZERO(&rdfs);
+        /*把标准输入的文件描述符加入到集合中*/
+        FD_SET(0, &rdfs);
+        maxfd = 0;
+        /*把当前连接的文件描述符加入到集合中*/
+        FD_SET(sock_cli, &rdfs);
+        /*找出文件描述符集合中最大的文件描述符*/
+        if(maxfd < sock_cli)
+            maxfd = sock_cli;
+        tv.tv_sec = 5;
+        tv.tv_usec = 0;
+        retval = select(maxfd+1,&rdfs,NULL,NULL,&tv);
+        if (retval == -1){
+            std::cout << "select Fail" << std::endl;
+            perror("select");
+            continue;
+        } else if(retval == 0){
+            std::cout << "接收到空的信息" << std::endl;
+            char buff[1024] = "you zenme bufa message";
+            send(sock_cli,buff,sizeof(buff),0);
+        } else{
+            if (FD_ISSET(sock_cli,&rdfs)){
+                char buff[1024];
+                int recv_ = recv(sock_cli,buff,sizeof(buff),0);
+                if (recv_ <= 0){
+                    std::cout << "读取到的数据失败 " << std::endl;
+                    break;
+                }
+                std::cout << "接收到的信息：" << buff << "长度：" << strlen(buff) << "recv_:" << recv_ << std::endl;
+                memset(buff, 0, sizeof(buff));
+            }
+            if(FD_ISSET(0, &rdfs))
+            {
+                char sendbuf[BUFFER_SIZE];
+                std::cout << "请输入数据" << std::endl;
+                fgets(sendbuf, sizeof(sendbuf), stdin);
+                send(sock_cli, sendbuf, strlen(sendbuf),0); //发送
+                memset(sendbuf, 0, sizeof(sendbuf));
+            }
+        }
+
+    }
+
+    return 0;
 }

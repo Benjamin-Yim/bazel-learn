60566583b8f1c5bbbcc5b3a96c79eaa6ec85c8a4
zhongya.yan <zhongya.yan@ly.com>
2020-05-10 10:14:12 +0800

    添加客户端请求 Demo

--- main/main.cc
+++ main/main.cc
@@ -14,66 +14,34 @@
 #include <sys/queue.h>
 
 int MAX_URL = 8192;
-void send_document_cb(struct evhttp_request *req, void *argv) {
-  // char *s = "";
-  const struct evhttp_uri *evhttp_uri = evhttp_request_get_evhttp_uri(req);
-  char url[MAX_URL];
-  evhttp_uri_join(const_cast<struct evhttp_uri *>(evhttp_uri), url, MAX_URL);
-  std::cout << "请求地址:" << url << std::endl;
 
-  evhttp_add_header(req->output_headers, "token", "来自远方的呼唤");
-  struct evbuffer *evbuf = evbuffer_new();
-  if (!evbuf) {
-    return;
+void response_cb(struct evhttp_request *req, void *argv) {
+  evbuffer *buff = evhttp_request_get_input_buffer(req);
+  size_t len = evbuffer_get_length(buff);
+  char data[1024];
+  bzero(&data, sizeof(data));
+  while (1) {
+    int n = evbuffer_remove(buff, data, 1024);
+    if (n <= 0) {
+      break;
+    }
+    std::cout << "接收到的数据" << data << std::endl;
   }
-
-  evbuffer_add_printf(evbuf, "来自服务端响应");
-  evhttp_send_reply(req, HTTP_OK, "Success", evbuf);
-  evbuffer_free(evbuf);
-}
-
-void http_cb(struct evhttp_request *req, void *arg) {
-  const struct evhttp_uri *evhttp_uri = evhttp_request_get_evhttp_uri(req);
-
-  char url[MAX_URL];
-  const char *host = evhttp_request_get_host(req);
-
-  evhttp_uri_join(const_cast<struct evhttp_uri *>(evhttp_uri), url, MAX_URL);
-
-  std::cout << "请求的URL" << url << std::endl;
-  struct evbuffer *evbuf = evbuffer_new();
-  if (!evbuf) {
-    std::cout << "申请缓存空间失败" << std::endl;
-    return;
-  }
-
-  evbuffer_add_printf(evbuf, "服务端接收到的地址 %s,给你回应了", url);
-
-  evhttp_send_reply(req, HTTP_OK, "Success", evbuf);
-  evbuffer_free(evbuf);
+  event_base_loopbreak((event_base *)argv);
 }
 
 int main(int argc, char **argv) {
-  int http_port = 8080;
-  char *http_address = "0.0.0.0";
+  char *server_addr = "0.0.0.0";
+  int port = 8080;
   struct event_base *base = event_base_new();
-  struct evhttp *http_server = evhttp_new(base);
-
-  if (NULL == http_server) {
-    std::cout << "新建 HTTP 服务器失败" << std::endl;
-    return -1;
-  }
-
-  int ebc = evhttp_bind_socket(http_server, http_address, http_port);
-
-  if (ebc < 0) {
-    std::cout << "启动服务器失败，绑定地址和端口失败" << std::endl;
-    return -1;
-  }
-
-  evhttp_set_cb(http_server, "/test", http_cb, NULL);
-  evhttp_set_gencb(http_server, send_document_cb, NULL);
+  struct evhttp_connection *connect =
+      evhttp_connection_base_new(base, NULL, server_addr, port);
+  struct evhttp_request *req = evhttp_request_new(response_cb, base);
+  evhttp_add_header(evhttp_request_get_output_headers(req), "host",
+                    "localhost");
+  evhttp_make_request(connect, req, EVHTTP_REQ_POST, "/test");
   event_base_dispatch(base);
-  evhttp_free(http_server);
+  evhttp_connection_free(connect);
+
   return 0;
 }
